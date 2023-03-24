8e70dc8c952e037b0b69291c8da388e098f54c15
Alejandro Gallego <alejandrogalldom@gmail.com>
2023-03-24 20:40:37 +0100

    eureka server ready

--- motorbikeService/src/main/java/ImAle/motorbikeService/MotorbikeServiceApplication.java
+++ motorbikeService/src/main/java/ImAle/motorbikeService/MotorbikeServiceApplication.java
@@ -2,8 +2,10 @@ package ImAle.motorbikeService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MotorbikeServiceApplication {
 
 	public static void main(String[] args) {
